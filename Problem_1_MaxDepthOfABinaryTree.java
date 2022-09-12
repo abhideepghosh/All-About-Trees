4d910d43d2651e02301467eff66e960ffa06bc55
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-09-12 12:53:04 +0530

    Update Problem_1_MaxDepthOfABinaryTree.java

--- Problem_1_MaxDepthOfABinaryTree.java
+++ Problem_1_MaxDepthOfABinaryTree.java
@@ -13,13 +13,27 @@
  *     }
  * }
  */
+
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Maximum Depth of Binary Tree.
+Memory Usage: 41.9 MB, less than 94.65% of Java online submissions for Maximum Depth of Binary Tree.
+*/
+
+// We Use Recursion To Find The Max Depth In The Most Efficient Way
 class Solution {
+    
     public int maxDepth(TreeNode root) {
+        
+        // Base Case Of Recursion
         if(root == null){
             return 0;
         }
-        int leftHeight = maxDepth(root.left);
-        int rightHeight = maxDepth(root.right);
-        return Math.max(leftHeight, rightHeight) + 1;
+        
+        // Calulating Height From The Leaf Nodes To The Current Nodes
+        int heightLeft = maxDepth(root.left);
+        int heightRight = maxDepth(root.right);
+        
+        // Calculating The Max Of Both Heights + 1
+        return Math.max(heightLeft, heightRight) + 1;
     }
 }
